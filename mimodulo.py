2f9b45ef80a53358db4c1d97f0b98f3864817a26
ErickelDuro <106211973+ErickelDuro@users.noreply.github.com>
2022-10-18 11:04:01 -0500

    Delete mimodulo.py

--- mimodulo.py
+++
@@ -1,8 +0,0 @@
-def raiz(a):
-    x = 0
-    while (x*x) <= a: x += 1
-    if abs(a - (x*x)) > abs(a - ((x-1)*(x-1))): x -= 1
-    return round((a + (x*x))/(2*x), 4)
-
-print(raiz(int(input("Ingresar el valor >>> "))))
-
